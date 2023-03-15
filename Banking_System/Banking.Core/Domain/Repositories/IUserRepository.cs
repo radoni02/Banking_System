c942de767e606bec1d39f47f0ba3a604e99c9a1e
radoni02 <108626591+radoni02@users.noreply.github.com>
2023-03-15 23:43:25 +0100

    Adding repository.

---
+++ Banking_System/Banking.Core/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Banking.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Core.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+        Task<User> GetAsync(Guid id);
+    }
+}
