22a0ba0677d5612cf4a4479ec9015f8d1c844308
radoni02 <108626591+radoni02@users.noreply.github.com>
2023-04-11 21:02:12 +0200

    Adding RemoveBalance command/handler.

---
+++ Banking_System/Banking.Application/Commands/Handlers/RemoveBalanceHandler.cs
@@ -0,0 +1,39 @@
+﻿using Banking.Application.Exceptions;
+using Banking.Core.Domain.Repositories;
+using Convey.CQRS.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Application.Commands.Handlers
+{
+    public class RemoveBalanceHandler : ICommandHandler<RemoveBalance>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IBankAccountRepository _bankAccountRepository;
+
+        public RemoveBalanceHandler(IUserRepository userRepository, IBankAccountRepository bankAccountRepository)
+        {
+            _userRepository = userRepository;
+            _bankAccountRepository = bankAccountRepository;
+        }
+
+        public async Task HandleAsync(RemoveBalance command, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var user = await _userRepository.GetAsync(command.UserId);
+            if(user is null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+            var account = user.Accounts.FirstOrDefault(x => x.Type == Core.Domain.Consts.AccountType.ForeignExchangeAccount);
+            if(account is null)
+            {
+                throw new AccountNotFoundException();
+            }
+            account.RemoveBalanceFromAccount(command.Currency);
+            await _bankAccountRepository.UpdateAsync(account);
+        }
+    }
+}
