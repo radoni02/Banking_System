c65bdd460e494a059f84235286289de0d6b82bd4
radoni02 <108626591+radoni02@users.noreply.github.com>
2023-04-20 16:22:02 +0200

    Adding rest of DTOs and queries.

---
+++ Banking_System/Banking.Application/Dto/BalanceDto.cs
@@ -0,0 +1,17 @@
+﻿using Banking.Core.Domain.Consts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Application.Dto
+{
+    public class BalanceDto
+    {
+       [Required] public decimal AccountBalance { get;  set; }
+
+       [Required] public Currency Currency { get; set; }
+    }
+}
---
+++ Banking_System/Banking.Application/Dto/OwnerDto.cs
@@ -0,0 +1,17 @@
+﻿using Banking.Core.Domain.Consts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Application.Dto
+{
+    public class OwnerDto
+    {
+        [Required] public string FirstName { get; set; }
+        [Required] public string LastName { get; set; }
+        [Required] public string EmailAddress { get; set; }
+    }
+}
