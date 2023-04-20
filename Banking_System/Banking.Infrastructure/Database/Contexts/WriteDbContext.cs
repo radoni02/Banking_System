dbbf52a489e8b8b74d1a9dab263c79d59280999a
radoni02 <108626591+radoni02@users.noreply.github.com>
2023-04-20 17:54:32 +0200

    Adding WriteDbContext

---
+++ Banking_System/Banking.Infrastructure/Database/Contexts/WriteDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Banking.Core.Domain.Entities;
+using Banking.Core.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Infrastructure.Database.Contexts
+{
+    internal sealed class WriteDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<BankTransfer> Transfers { get; set; }
+        public DbSet<Money> Balances { get; set; }
+        public DbSet<BankingCard> BankingCards { get; set; }
+
+        public WriteDbContext(DbContextOptions<WriteDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("Banking System");
+        }
+    }
+}
