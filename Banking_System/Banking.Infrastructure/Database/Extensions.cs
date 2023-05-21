d21437de675927214cb3339d6451f442913e4659
radoni02 <108626591+radoni02@users.noreply.github.com>
2023-05-21 17:46:31 +0200

    Adding ConnectionString configuration

---
+++ Banking_System/Banking.Infrastructure/Database/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using Banking.Infrastructure.Database.Contexts;
+using Banking.Infrastructure.Database.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Infrastructure.Database
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddPostgres(this IServiceCollection services,IConfiguration configuration)
+        {
+            var options = configuration.GetOptions<PostgresOptions>("Postgres");
+            services.AddDbContext<ReadDbContext>(ctx => ctx.UseNpgsql(options.ConnectionString));
+            services.AddDbContext<WriteDbContext>(ctx => ctx.UseNpgsql(options.ConnectionString));
+            return services;
+        }
+    }
+}
---
+++ Banking_System/Banking.Infrastructure/Database/Options/PostgresOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Infrastructure.Database.Options
+{
+    public class PostgresOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}
